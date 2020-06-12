d0b8ab88ee1df7cfc8c284508b02224b49e878e2
Greg van Brug <gvb@apostrophecms.com>
2020-06-12 16:20:40 -0400

    formatting

--- test/markdown.js
+++ test/markdown.js
@@ -5,7 +5,9 @@ const utils = require("../lib/markdown");
 describe("apostrophe-guides:markdown", function() {
 
   it("should render markdown", function(done) {
-    const expected = '<h1 id="title">Title</h1>\n';
+    const expected = `<h1 id="title" class="apos-guide-heading">
+  <a class="apos-guide-heading-link" href="#title">Title</a>
+</h1>`;
 
     const md = "# Title";
     const actual = utils.renderDoc(md);
