d123d3e36a69a9b6d8b4861027f5be4bf9f661f6
Greg van Brug <gvb@apostrophecms.com>
2020-06-22 16:46:07 -0400

    don't need to make CSRF exceptions

--- index.js
+++ index.js
@@ -7,14 +7,9 @@ module.exports = {
 
   afterConstruct: self => {
     self.addRoutes();
-    self.apos.on("csrfExceptions", self.addCsrfException);
   },
 
   construct: (self, options) => {
-    self.addCsrfException = exceptions => {
-      exceptions.push(`/${options.path}/**`);
-    };
-
     require("./lib/guide")(self, options);
 
     self.pushAsset("stylesheet", "always", { when: "always" });
